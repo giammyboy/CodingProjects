3d0fd9c964f6dcdde78a5d2a7d71ea4aae561650
Gianmarco Rossi <rossigianmarco29@gmail.com>
2023-10-15 17:32:34 +0200

    test commit

--- C++Projects/fdp/fdi/cmake-build-debug/cmake_install.cmake
+++ C++Projects/fdp/fdi/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/giammy/CLionProjects/fdp/fdi
+# Install script for directory: /Users/giammy/Documents/CodingProjects/C++Projects/fdp/fdi
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/giammy/CLionProjects/fdp/fdi/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/giammy/Documents/CodingProjects/C++Projects/fdp/fdi/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- C++Projects/fdp/fdi/main.cpp
+++ C++Projects/fdp/fdi/main.cpp
@@ -7,24 +7,6 @@ using namespace std;
 
 
 int main(){
-    vivaio V;
-    InizializzaVivaio(V, 3);
-    stampaVivaio(V);
-    cout << "---------------------" << endl;
-    piantaVivaio(V, "pino", 2);
-    piantaVivaio(V, "abete", 1);
-    piantaVivaio(V, "cipresso", 3);
-    stampaVivaio(V);
-    cout << "---------------------" << endl;
-    piantaVivaio(V, "tiglio", 2);
-    stampaVivaio(V);
-    cout << "---------------------" << endl;
-//    eliminaPiante(V,2,2);
-    stampaVivaio(V);
-    cout << "---------------------" << endl;
-//    cout << quantePiante(V, "tiglio") << endl;
-    cout << "---------------------" << endl;
-//    Vivaio W = copiaVivaio(V);
-//    stampaVivaio(W);
+    cout << "hello" << endl;
     return 0;
 }
