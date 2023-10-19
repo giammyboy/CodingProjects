803c5aec9546413b2444270a05add806d437910b
Gianmarco Rossi <rossigianmarco29@gmail.com>
2023-10-20 00:01:50 +0200

    test

---
+++ Rust/test_project/target/debug/deps/test_project-749a6d5e3e64ba63.d
@@ -0,0 +1,9 @@
+/Users/giammy/Documents/CodingProjects/Rust/test_project/target/debug/deps/test_project-749a6d5e3e64ba63.rmeta: src/main.rs Cargo.toml
+
+/Users/giammy/Documents/CodingProjects/Rust/test_project/target/debug/deps/test_project-749a6d5e3e64ba63.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
